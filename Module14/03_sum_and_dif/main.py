bf6a221aba960948563f618c1381b0ee7ad177ed
Alex <sturmmanngr@gmail.com>
2021-04-23 20:27:58 +0700

    дз 14 сделано (3 задание переделано)

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,14 +1,21 @@
+def summary(n):
+    sum = 0
+    for i in range(len(str(n))):
+        sum += n % 10
+        n //= 10
+    return sum
+
+
+def quantity(n):
+    counts = 0
+    for i in range(len(str(n))):
+        counts += 1
+    return counts
+
+
 N = int(input('Введите целое положительное число N: '))
-
-n_sum = 0
-n_counts = 0
-N_str = str(N)
-
-
-for i in range(len(N_str)):
-    n_sum += N % 10
-    n_counts += 1
-    N //= 10
+n_sum = summary(N)
+n_counts = quantity(N)
 
 print('Сумма цифр равна', n_sum)
 print('Кол-во цифр в числе:', n_counts)
