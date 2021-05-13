de6f47fe1f213b26f45fa701010861667de37cea
Табарков Алексей <ssturmmann@yandex.ru>
2021-05-13 02:10:13 +0000

    Update 18_6 main.py

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,9 +1,13 @@
 start_string = input('Введите строку: ')
 result = ''
-i = 0
-while i < len(start_string):
-  result += start_string[i]
-  lenght = start_string.count(start_string[i])
-  result += str(lenght)
-  i += lenght
+count = 0
+prev = start_string[0]
+for letter in start_string:
+  if letter == prev:
+    count += 1
+    prev = letter
+  else:
+    result += prev
+    result += str(count)
+    count = 1
 print(result)
