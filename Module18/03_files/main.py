a246b1611c261afa55183ede308e87cd4d3395b2
Табарков Алексей <ssturmmann@yandex.ru>
2021-05-13 02:07:48 +0000

    Update 18_3 main.py

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,7 +1,8 @@
 file_name = input('Введите название файла: ')
+suffix = ('.txt', '.docx')
 if file_name[0] in '@№$%^&*().':
   print('Ошибка: название начинается на один из специальных символов')
-elif file_name.endswith('.txt') or file_name.endswith('.docx'):
+elif file_name.endswith(suffix):
   print('Файл назван верно.')
 else:
   print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
