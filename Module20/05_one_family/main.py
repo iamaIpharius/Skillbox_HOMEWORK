2924caa21332c33cfd6348dd282b882fb9c06e61
Табарков Алексей <ssturmmann@yandex.ru>
2021-05-21 08:03:49 +0000

    20 5/10

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def get_family_members(family_dict, surname):
+    for name, age in family_dict.items():
+        if name.startswith(surname):
+            print(name, age)
+
+
+my_dict = {'Cидорова Анна': 23, "Иванова Лена": 22, "Петрова Катя": 55}
+
+surname = input('Введите фамилию: ')
+
+get_family_members(my_dict, surname)
