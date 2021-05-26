3d23c012f23446905705d268462d944e916c2806
Табарков Алексей <ssturmmann@yandex.ru>
2021-05-26 03:46:29 +0000

    20 6/10 переделано

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -3,10 +3,9 @@ import random
 ran_list = [random.randint(1, 10) for _ in range(10)]
 print(ran_list)
 # первый вариант
-new_list = []
-for i in range(0, len(ran_list), 2):
-    tup = ran_list[i], ran_list[i + 1]
-    new_list.append(tup)
+new_list = [(ran_list[i], ran_list[i + 1]) for i in range(0, len(ran_list), 2)]
+
+
 # второй вариант
 first_list = [ran_list[i] for i in range(0, len(ran_list), 2)]
 second_list = [ran_list[i + 1] for i in range(0, len(ran_list), 2)]
