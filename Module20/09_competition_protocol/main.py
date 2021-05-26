e6cc8b1cd71672604aecf41c44c73e4c99175a9f
Табарков Алексей <ssturmmann@yandex.ru>
2021-05-26 04:22:17 +0000

    20 10/10 переделано

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,34 +1,21 @@
+def score_key(a):
+    return a[1][0]*100000000 - a[1][1]
+
 count_of_protocol = int(input('Сколько записей в протоколе? '))
-result_dict = dict()
+score_table = dict()
 print('Введите записи (результат и имя):')
+
 for i in range(1, count_of_protocol + 1):
     scribe = input('{0} запись: '.format(i))
     result = scribe.split()
-    result_dict[i] = result
+    result[0] = int(result[0])
+    if result[1] in score_table:
+        if result[0] > score_table[result[1]][0]:
+            score_table[result[1]] = [result[0], i]
+    else:
+        score_table[result[1]] = [int(result[0]), i]
+scores = list(score_table.items())
 
-first_place_score = 0
-first_place = []
-for position, score in result_dict.items():
-    if int(score[0]) > first_place_score:
-        first_place_score = int(score[0])
-        first_place = score
-
-second_place_score = 0
-second_place = []
-for position, score in result_dict.items():
-    if first_place_score >= int(score[0]) > second_place_score and score[1] != first_place[1]:
-        second_place_score = int(score[0])
-        second_place = score
-
-third_place_score = 0
-third_place = []
-for position, score in result_dict.items():
-    if second_place_score >= int(score[0]) > third_place_score and score[1] != first_place[1] and score[1] != \
-            second_place[1]:
-        third_place_score = int(score[0])
-        third_place = score
-
-print('1 место. {name} ({score})'.format(name=first_place[1], score=first_place[0]))
-print('2 место. {name} ({score})'.format(name=second_place[1], score=second_place[0]))
-print('3 место. {name} ({score})'.format(name=third_place[1], score=third_place[0]))
-
+scores.sort(key=score_key, reverse=True)
+for i in range(3):
+    print('{place} место. {name} ({score})'.format(place=i+1, name=scores[i][0], score=scores[i][1][0]))
