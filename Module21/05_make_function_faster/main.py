28fdb809f756ab4ed271d1722ecd23f60008443b
Табарков Алексей <ssturmmann@yandex.ru>
2021-06-08 08:32:01 +0000

    задание 5 сделано

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,10 @@
-def calculating_math_func(data):
+def calculating_math_func(data, data_dict={}):
+    if data in data_dict:
+        return data_dict[data]
     result = 1
     for index in range(1, data + 1):
         result *= index
     result /= data ** 3
     result = result ** 10
+    data_dict[data] = result
     return result
-
-# TODO оптимизировать функцию
