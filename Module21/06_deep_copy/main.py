01fffcd27d3ba019f8dfa76acaefddac016c0724
Табарков Алексей <ssturmmann@yandex.ru>
2021-06-08 05:00:43 +0000

    задание 6 переделано

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -1,10 +1,10 @@
 site = {
     'html': {
         'head': {
-            'title': 'Куплю/продам {product} недорого'
+            'title': 'Куплю/продам телефон недорого'
         },
         'body': {
-            'h2': 'У нас самая низкая цена на {product}',
+            'h2': 'У нас самая низкая цена на телефон',
             'div': 'Купить',
             'p': 'продать'
         }
@@ -12,15 +12,35 @@ site = {
 }
 
 
-def get_new_sites(site, sites_ad):
-    if sites_ad == 0:
+def get_new_sites(struct, key, name):
+    if count == 0:
         return
-    name = input('Введите название продукта: ')
-    site['html']['head']['title'] = site['html']['head']['title'].format(product=name)
-    site['html']['body']['h2'] = site['html']['body']['h2'].format(product=name)
+    for s_key, s_item in struct.items():
+        if isinstance(s_item, str):
+            if key in s_item:
+                temp = s_item.replace(key, name)
+                struct[s_key] = temp
+                return
+            else:
+                return
+        elif isinstance(s_item, dict):
+            get_new_sites(s_item, key, name)
+
+
+def nice_print(struct, count=1):
+    for s_key, s_value in struct.items():
+        if isinstance(s_value, str):
+            print('/'*count, s_key, ':', s_value)
+        elif isinstance(s_value, dict):
+            print('/'*count, s_key, ':')
+            nice_print(s_value, count=count+1)
+
+
+count = int(input('Сколько будет сайтов? '))
+
+for _ in range(count):
+    old = input('Введите название старого продукта: ')
+    new = input('Введите название нового продукта: ')
+    get_new_sites(site, old, new)
     print(site)
-    sites_ad -= 1
-    get_new_sites(site, sites_ad)
-
-
-get_new_sites(site, 2)
+    nice_print(site)
