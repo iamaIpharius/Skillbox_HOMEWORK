b7f54be6d35c58d99e085ef7478241fadba1f031
Табарков Алексей <ssturmmann@yandex.ru>
2021-06-21 09:39:29 +0000

    задание 1 сделано

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+with open('people.txt', 'r') as text_file:
+    letter_count = 0
+    for index, line in enumerate(text_file):
+        try:
+            if len(line) < 4:
+
+                for letter in line:
+                    if letter.isalpha():
+                        letter_count += 1
+                with open('errors.txt', 'a', encoding='utf-8') as log_file:
+                    log_file.write(f'Длина менее 3 символов в {index} строке\n')
+                raise IndexError
+            for letter in line:
+                if letter.isalpha():
+                    letter_count += 1
+        except IndexError:
+            print(f'Длина менее 3 символов в {index} строке')
+
+print(letter_count)
+print(text_file.closed)
+print(log_file.closed)
