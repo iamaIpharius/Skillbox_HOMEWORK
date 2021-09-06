b4b4823f05c35ea1f622e17b8d8247e71cdada1c
Табарков Алексей <ssturmmann@yandex.ru>
2021-09-06 06:25:37 +0000

    задание 3 сделано

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+import os
+
+
+def gen_files_path(path=os.path.join('C:')):
+    for i in os.listdir(path):
+        cur_path = os.path.join(path, i)
+        if os.path.isfile(cur_path):
+            yield cur_path
+        elif os.path.isdir(cur_path):
+            for q in gen_files_path(cur_path):
+                print(q)
+
+
+os.path.join(os.path.abspath('.').split(os.path.sep)[0])
+
+for q in gen_files_path():
+    print(q)
