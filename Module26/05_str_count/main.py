40a479c88df5e33ce16b3f11bebe25ce3ccc1976
Табарков Алексей <ssturmmann@yandex.ru>
2021-09-07 07:35:53 +0000

    задание 5 сделано

--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+import os
+
+
+def gen_files_path(path=os.path.join('C:')):
+    for i in os.listdir(path):
+        cur_path = os.path.join(path, i)
+        if os.path.isfile(cur_path) and cur_path.endswith('.py'):
+            print(cur_path)
+            with open(cur_path, 'r', encoding='utf-8') as py_file:
+                for line in py_file:
+                    if line.isspace() or '#' in line:
+                        continue
+                    else:
+                        yield 1
+
+
+        elif os.path.isdir(cur_path):
+            for q in gen_files_path(cur_path):
+                yield q
+
+
+my_path = os.path.join(input('Введие путь: '))
+total_lines = 0
+for q in gen_files_path(my_path):
+    total_lines += q
+
+print(f'В найденных питоновских файлах {total_lines} строк кода без пустых и комментариев')
