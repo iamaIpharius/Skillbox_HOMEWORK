9ad287d9b860637db980052c4e9ea23f339ce14a
Табарков Алексей <ssturmmann@yandex.ru>
2021-09-08 07:13:14 +0000

    5 done

--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,7 +1,7 @@
 import os
+from collections.abc import Iterable
 
-
-def gen_files_path(path=os.path.join('C:')):
+def get_line(path: str = os.path.join('C:')) -> Iterable[int]:
     for i in os.listdir(path):
         cur_path = os.path.join(path, i)
         if os.path.isfile(cur_path) and cur_path.endswith('.py'):
@@ -15,13 +15,13 @@ def gen_files_path(path=os.path.join('C:')):
 
 
         elif os.path.isdir(cur_path):
-            for q in gen_files_path(cur_path):
+            for q in get_line(cur_path):
                 yield q
 
 
 my_path = os.path.join(input('Введие путь: '))
 total_lines = 0
-for q in gen_files_path(my_path):
+for q in get_line(my_path):
     total_lines += q
 
 print(f'В найденных питоновских файлах {total_lines} строк кода без пустых и комментариев')
