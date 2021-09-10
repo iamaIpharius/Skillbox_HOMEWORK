a234cac04e44243324fc23b5ad35478350036225
Табарков Алексей <ssturmmann@yandex.ru>
2021-09-10 04:03:39 +0000

    Update 3

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -1,7 +1,18 @@
 import os
+from collections.abc import Iterable
 
 
-def gen_files_path(path: str = os.path.join('C:')) -> str:
+def find_path(to_find: str, start: str = os.path.join('C:')) -> None:
+    for i in os.listdir(start):
+        current = os.path.join(start, i)
+        if i == to_find and os.path.isdir(current):
+            for q in gen_files_path(current):
+                print(q)
+        elif os.path.isdir(current):
+            find_path(to_find, current)
+
+
+def gen_files_path(path: str) -> str:
     for i in os.listdir(path):
         cur_path = os.path.join(path, i)
         if os.path.isfile(cur_path):
@@ -11,7 +22,7 @@ def gen_files_path(path: str = os.path.join('C:')) -> str:
                 yield q
 
 
-my_path = os.path.join(input('Введите путь: '))
+my_path = os.path.abspath(input('Введите директорию, где будем искать: '))
+find = input('Какой каталог ищем? ')
 
-for q in gen_files_path(my_path):
-    print(q)
+find_path(find, my_path)
