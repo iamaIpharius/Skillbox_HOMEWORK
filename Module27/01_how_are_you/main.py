12bbfbdeebf740770734011e71c2bf7f44fd0ca5
Табарков Алексей <ssturmmann@yandex.ru>
2021-09-16 08:21:51 +0000

    задание 1

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+import time, random, functools
+from typing import Callable, Any
+
+
+def how_are_you(f: Callable) -> Callable:
+    """
+    Декоратор. Задает назойливый вопрос
+    :param f: передается функция
+    :return: возвращает функцию
+    """
+    input('Как дела?')
+    print('А у меня не очень!\nЛадно, держи свою функцию...')
+    return f
+
+
+@how_are_you
+def test() -> None:
+    """
+    Тестовая функция
+    :return: печатает в консоль что-то
+    """
+    print('<Тут что-то происходит...>')
+
+
+test()
