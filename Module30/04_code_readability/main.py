1b251647669bdd0743aa8f6fa30a75eace07374f
Табарков Алексей <ssturmmann@yandex.ru>
2021-10-13 07:17:53 +0000

    задание 4

--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+from typing import List
+
+print([x for x in range(1, 1001)]) #однострочное решение
+
+result: List = [] #простое решение
+for count in range(1, 1001):
+    result.append(count)
+print(result)
