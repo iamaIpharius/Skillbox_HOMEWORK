87d4d02dc984795ea3ba52ed7d7107fba83a994f
Табарков Алексей <ssturmmann@yandex.ru>
2021-10-13 07:11:10 +0000

    задание 2

--- Module30/02_timeit/main.py
+++ Module30/02_timeit/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import timeit
+
+
+res: float = timeit.timeit('"".join(str(n) for n in range(100))', number=10000)
+res1: float = timeit.timeit("''.join([str(a) for a in range(100)])", number=10000)
+res2: float = timeit.timeit("''.join(map(lambda x: str(x), range(100)))", number=10000)
+
+
+print(res)
+print(res1)
+print(res2)
+
