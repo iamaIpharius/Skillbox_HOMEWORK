ccb377ae7c888f381e322609ebba051055e82ea7
Табарков Алексей <ssturmmann@yandex.ru>
2021-10-13 09:35:57 +0000

    Update 5

--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -1,30 +1,10 @@
-from collections import deque
+from collections import Counter
 
 
 def can_be_poly(string: str) -> bool:
-    d_right = deque('')
-    d_left = deque('')
-    for elem in string.lower():
-        if elem not in ' !@#$%^&*()_+"№;:?':
-            d_right.append(elem)
-            d_left.appendleft(elem)
-
-    if d_left == d_right:
-        return True
-    return False
+    return len(list(filter(lambda x: x % 2, Counter(string).values()))) <= 2
 
 
-def can_be_poly2(string: str) -> bool:
-    d_normal = deque(filter(lambda x: x not in ' !@#$%^&*()_+"№;:?', string.lower()))
-    d_reverse = deque([x for x in reversed(d_normal)])
+print(can_be_poly('ababc'))
 
-    if d_normal == d_reverse:
-        return True
-    return False
-
-
-print(can_be_poly('saippuakivikauppias!'))
-print(can_be_poly('а роза упала на лапу Азора'))
-print(can_be_poly2('saippuakivikauppias!'))
-print(can_be_poly2('а роза упала на лапу Азораff'))
-
+print(can_be_poly('abbbc'))
