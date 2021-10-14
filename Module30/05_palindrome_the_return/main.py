7872d9bbd1965f6229199d966a837b6b85dc6e88
Табарков Алексей <ssturmmann@yandex.ru>
2021-10-14 01:20:49 +0000

    Update 5

--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 
 def can_be_poly(string: str) -> bool:
-    return len(list(filter(lambda x: x % 2, Counter(string).values()))) <= 2
+    return len(list(filter(lambda x: x % 2, Counter(string).values()))) < 2
 
 
 print(can_be_poly('ababc'))
