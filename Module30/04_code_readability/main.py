d7b25a1c38a33817f6156b9a0ceac04801d2b2f1
Табарков Алексей <ssturmmann@yandex.ru>
2021-10-14 04:37:13 +0000

    Переделано 4

--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,8 +1,14 @@
-from typing import List
+print(list(filter(lambda x: all(map(lambda i: x % i != 0, range(2, int(x ** 0.5) + 1))), range(2, 1001))))
 
-print([x for x in range(1, 1001)]) #однострочное решение
+simple_list = []
 
-result: List = [] #простое решение
-for count in range(1, 1001):
-    result.append(count)
-print(result)
+for x in range(2, 1001):
+    flag = True
+    for y in range(2, x):
+        if x % y == 0:
+            flag = False
+            break
+    if flag:
+        simple_list.append(x)
+
+print(simple_list)
